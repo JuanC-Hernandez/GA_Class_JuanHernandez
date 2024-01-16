bcab107ee679cd7eb6e07770f9695090f8a9e24a
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-15 21:31:45 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GA_Class_JuanHernandez
+{
+    internal class Program
+    {
+        // Juan Hernandez
+        // Guided Assignment: Class
+        //01/15/2024
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Student juan;
+            Student jack;
+            Professor will;
+            Car reserved_space1;
+        }
+    }
+}
